7bb01c66d893791eb52c42914aaa90dfeac925ea
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-12 20:55:14 -0300

    Removido a tela de loading da Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,11 @@
 'use client'
 
-import LoadingScreen from '@/components/loading/AbnerDevLoader'
-import { useEffect, useState } from 'react'
-
 export default function HomePage() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false)
-    }, 4800) // 4.8 segundos
-
-    return () => clearTimeout(timeout)
-  }, [])
 
   return (
     <>
-      {loading && <LoadingScreen />}
-      {!loading && (
-        <>
-          
-          <main className='min-h-screen pt-[10vh]'>
-          </main>
-        </>
-      )}
+      <main className='min-h-screen py-[15vh]'>
+      </main>
     </>
   )
 }

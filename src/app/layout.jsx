6c90184f1cde17ce37cf423cec911f67223fc5d5
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-12 20:54:55 -0300

    Add scroll progress no layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,35 @@
-import { Space_Mono, JetBrains_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header/Header";
+import Footer from '@/components/footer/Footer'
+import Header from '@/components/header/Header'
+import LoaderProvider from '@/components/loaderProvider/LoaderProvider'
+import { JetBrains_Mono } from 'next/font/google'
+import './globals.css'
+import ScrollLoader from '@/components/scrollLoader/Scroll'
 
 const jetBrains = JetBrains_Mono({
   weight: ['400'],
-  subsets: ["latin"],
+  subsets: ['latin'],
   display: 'swap',
-});
+})
 
 export const metadata = {
-  title: "Abner Ferreira | Dev Front-end",
-  description: "Portfólio de Abner Rodrigues Ferreira, desenvolvedor front-end especializado em ReactJS, NextJS. Experiência em e-commerces, UX/UI, acessibilidade e performance web com foco em boas práticas e design system. Portfolio of Abner Rodrigues Ferreira, front-end developer specialized in ReactJS, NextJS. Experienced in e-commerce, UX/UI, accessibility, and web performance with a strong focus on best practices and design systems.",
-};
+  title: 'Abner Ferreira | Dev Front-end',
+  description:
+    'Portfólio de Abner Rodrigues Ferreira, desenvolvedor front-end especializado em ReactJS, NextJS. Experiência em e-commerces, UX/UI, acessibilidade e performance web com foco em boas práticas e design system. Portfolio of Abner Rodrigues Ferreira, front-end developer specialized in ReactJS, NextJS. Experienced in e-commerce, UX/UI, accessibility, and web performance with a strong focus on best practices and design systems.',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${jetBrains.className} min-h-screen`}>
-      <Header />
-        {children}
+    <html lang='en'>
+      <body
+        className={`${jetBrains.className} min-h-screen overflow-x-hidden selection:bg-[var(--yellow)] selection:text-[var(--black)] `}
+      >
+        <LoaderProvider>
+          <Header />
+          <ScrollLoader />
+          {children}
+          <Footer />
+        </LoaderProvider>
       </body>
     </html>
-  );
+  )
 }

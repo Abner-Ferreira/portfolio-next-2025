6a0eea7ce725080f12620dfab9b08087de7ecd1f
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-12 20:54:33 -0300

    Scroll progress finalizado

---
+++ src/components/scrollLoader/Scroll.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { motion, useScroll } from "framer-motion"
+
+export default function ScrollLoader() {
+
+  const { scrollYProgress } = useScroll()
+  
+  return (
+    <>
+      <motion.div
+        id='scroll-indicator'
+        style={{
+          scaleX: scrollYProgress,
+          position: 'fixed',
+          top: '10vh',
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          backgroundColor: 'var(--darkYellow)',
+        }}
+      />
+    </>
+  )
+}

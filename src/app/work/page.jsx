fcab9cc039b0483e67f03218cbd79df36d3c76e5
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-12 22:11:01 -0300

    Add os componentes de secao de projetos

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,19 +1,20 @@
+import ProjectSectionLeft from '@/components/projectSection/ProjectSectionLeft'
+import ProjectSectionRight from '@/components/projectSection/ProjectSectionRight'
+import { projects } from '@/data/projects'
+
+
+
 export default function Work() {
   return (
     <>
-      <main className='min-h-screen py-[15vh]'>
-        <div className='flex items-center justify-center min-h-screen bg-black px-4'>
-          <div className='perspective '>
-            <img
-              src='/GreenDrive.png'
-              alt='3D styled screen'
-              className='rounded-2xl shadow-2xl w-full h-auto max-w-[500px] sm:max-w-[600px] md:max-w-[700px] transform transition-transform duration-700'
-              style={{
-                transform: 'rotateX(10deg) rotateY(15deg)',
-              }}
-            />
-          </div>
-        </div>
+      <main className='min-h-screen py-[15vh] flex flex-col justify-center items-center'>
+        {projects.map((project, idx) =>
+          project.side === 'right' ? (
+            <ProjectSectionRight key={idx} {...project} />
+          ) : (
+            <ProjectSectionLeft key={idx} {...project} />
+          )
+        )}
       </main>
     </>
   )

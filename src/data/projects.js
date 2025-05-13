a9ca50e4540a8067016e6b0771c1e9fee04d3086
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-12 22:09:44 -0300

    Add 2 projetos

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    name: "GreenDrive",
+    side: "right",
+    img: "/greendrive.png",
+    link: "https://green-drive.vercel.app/",
+    problem: "Automotive companies and eco-conscious consumers alike struggle to find accessible, engaging content focused on sustainable mobility solutions.",
+    solution: "Built a platform that delivers informative, AI-generated content about sustainable automotive technologies, helping users explore eco-friendly solutions without needing technical expertise."
+  },
+  {
+    name: "Easy Bank",
+    side: "left",
+    img: "/easybank.png",
+    link: "https://abner-easy-bank.vercel.app/",
+    problem: "Automotive companies and eco-conscious consumers alike struggle to find accessible, engaging content focused on sustainable mobility solutions.",
+    solution: "Built a platform that delivers informative, AI-generated content about sustainable automotive technologies, helping users explore eco-friendly solutions without needing technical expertise."
+  },
+]

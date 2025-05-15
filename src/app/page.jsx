cd35d6b76dbe54080f13af7503bec4566cf616e9
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-15 14:59:37 -0300

    Pagina principal alterada

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 'use client'
 
-export default function HomePage() {
+import FeaturedSection from '@/components/featured-projects/featuredProjects'
+import ToolSection from '@/components/tool-section/Tool'
 
+export default function HomePage() {
   return (
     <>
       <main className='min-h-screen py-[15vh]'>
+        <FeaturedSection />
+        <ToolSection />
       </main>
     </>
   )

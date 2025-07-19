50167a23e5f40f1f929a63d04e1dc97afd23a3cb
Abner-Ferreira <abnerferr2015@gmail.com>
2025-07-19 01:05:09 -0300

    Add a Home na pagina principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 'use client'
 
 import FeaturedSection from '@/components/featured-projects/featuredProjects'
+import Home from '@/components/home/Home'
 import ToolSection from '@/components/tool-section/Tool'
 
 export default function HomePage() {
   return (
     <>
-      <main className='min-h-screen py-[15vh]'>
+      <main className='min-h-screen py-[10vh]'>
+        <Home />
         <FeaturedSection />
         <ToolSection />
       </main>

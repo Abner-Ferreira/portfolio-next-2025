79c3d6a16dc4f38ee21c31204880c14a21e5b6a3
Abner-Ferreira <abnerferr2015@gmail.com>
2025-07-21 15:47:49 -0300

    Alterado o tempo de carregamento do loader

--- src/components/loader-provider/LoaderProvider.jsx
+++ src/components/loader-provider/LoaderProvider.jsx
@@ -12,7 +12,7 @@ export default function LoaderProvider({ children }) {
     setLoading(true)
     const timeout = setTimeout(() => {
       setLoading(false)
-    }, 3000) // tempo da animação
+    }, 2500) // tempo da animação
 
     return () => clearTimeout(timeout)
   }, [pathname])
